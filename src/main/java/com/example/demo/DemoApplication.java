5a65e6e1dc9fdce8100cfdd4c6a74cdc2af7f781
bala-amypo <bala@amypo.in>
2025-12-19 07:49:17 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,14 @@
-package com.example.demo.servlet;
+package com.example.demo;
 
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
-import java.io.IOException;
+@SpringBootApplication
+@ServletComponentScan
+public class DemoApplication {
 
-@WebServlet(urlPatterns = "/simple-status")
-public class SimpleStatusServlet extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest req,
-                         HttpServletResponse resp)
-            throws IOException {
-
-        resp.setStatus(HttpServletResponse.SC_OK);
-        resp.getWriter().write("OK");
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
