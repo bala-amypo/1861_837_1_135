02de6869c68c43afb50b037d79e49d760d12c09b
bala-amypo <bala@amypo.in>
2025-12-23 07:43:12 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +20,17 @@ public class SwaggerConfig {
         httpsServer.setUrl("https://9188.408procr.amypo.ai/");
         httpsServer.setDescription("Amypo HTTPS Server");
 
+        // JWT Security Scheme
+        SecurityScheme jwtScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
         return new OpenAPI()
-                .servers(List.of(httpsServer));
+                .servers(List.of(httpsServer))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", jwtScheme))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("BearerAuth"));
     }
 }
